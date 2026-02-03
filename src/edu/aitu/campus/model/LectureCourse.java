05ee55fe11401d4f9244f3c184ed9b3ec51f7bd9
Saniya <saniazumadilla4@gmail.com>
2026-02-03 20:48:21 +0600

    Add course type models: LectureCourse, LabCourse, OnlineCourse

---
+++ src/edu/aitu/campus/model/LectureCourse.java
@@ -0,0 +1,7 @@
+package edu.aitu.campus.model;
+
+public class LectureCourse extends Course {
+    public LectureCourse(String title, int capacity, String day, String time) {
+        super(title, capacity, day, time);
+    }
+}
---
+++ src/edu/aitu/campus/model/OnlineCourse.java
@@ -0,0 +1,7 @@
+package edu.aitu.campus.model;
+
+public class OnlineCourse extends Course {
+    public OnlineCourse(String title, int capacity, String day, String time) {
+        super(title, capacity, day, time);
+    }
+}
